be934bfcb97bdb2d25450d6e6fe3c0b8fae53353
Facundo <facumolinacorujo@gmail.com>
2025-03-15 12:59:32 -0300

    Actualizado

--- src/main/java/org/example/Concursante.java
+++ src/main/java/org/example/Concursante.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Concursante {
+    private String nombre;
+    private int puntos;
+
+    public Concursante(String n) {
+        if (n == null || n.isEmpty()) {
+            throw new IllegalArgumentException("El nombre no puede ser nulo ni estar vacio");
+        }
+        nombre = n;
+        puntos = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Concursante that = (Concursante) o;
+        return Objects.equals(nombre, that.nombre);
+    }
+
+
+    void sumarPuntos(int sp) {
+        puntos += sp;
+    }
+
+    public int verPuntos() {
+        return puntos;
+    }
 }
--- src/main/java/org/example/Concurso.java
+++ src/main/java/org/example/Concurso.java
@@ -6,13 +6,16 @@ import java.util.List;
 
 public class Concurso {
     public List<Concursante> concursantes;
+    public String nombre;
     private LocalDate fechainicio;
     private LocalDate fechafin;
 
 
-    public Concurso(LocalDate fi, LocalDate ff) {
+    public Concurso(LocalDate fi, LocalDate ff, String n) {
+        nombre = n;
         fechainicio = fi;
         fechafin = ff;
+        concursantes = new ArrayList<>();
     }
 
     public void inscribir(Concursante c) {
@@ -30,8 +33,12 @@ public class Concurso {
 
     }
 
-    public List<Concursante> obtenerConcursantes() {
-        return new ArrayList<>(concursantes);
+
+    public int cantidadInscriptos() {
+        return this.concursantes.size();
     }
 
+    public boolean estaInscripto(Concursante c) {
+        return this.concursantes.contains(c);
+    }
 }
